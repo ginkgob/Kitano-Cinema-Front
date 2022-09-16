da3a55a0316ada79d00840bc4ab94ad3260c4a0a
ginkgo <alexvid33@gmail.com>
2022-09-16 14:45:50 +0200

    clean code

--- src/components/Cinema.jsx
+++ src/components/Cinema.jsx
@@ -1,63 +1,65 @@
 import React, { useEffect, useState } from 'react'
 
-export const Cinema = ( {sessionPrice, sessionName} ) => {
+export const Cinema = ( {sessionPrice} ) => {
   
 
-const[isSelected, setIsSelected] = useState(false)
+  const[isSelected, setIsSelected] = useState(false)
 
-const selectedSeat = (id) => {
-  isSelected[`${id}`] ? subtractSeat() : addSeat(); 
-  setIsSelected(prevState => ({...isSelected, [id]: !prevState[id]}))
-}
+  const selectedSeat = (id) => {
+    isSelected[`${id}`] ? subtractSeat() : addSeat(); 
+    setIsSelected(prevState => ({...isSelected, [id]: !prevState[id]}))
+  }
 
-const[clicks, setClicks] = useState(0)
-const [price, setPrice] = useState(0)
+  const[clicks, setClicks] = useState(0)
+  const [price, setPrice] = useState(0)
 
-const addSeat = () => {
-  setClicks( clicks + 1 );
-  setPrice( ( clicks + 1 ) * sessionPrice );
-}
-const subtractSeat = () => {
-  setClicks( clicks - 1 );
-  setPrice( ( clicks * sessionPrice ) - sessionPrice );
-}
+  const addSeat = () => {
+    setClicks( clicks + 1 );
+    setPrice( ( clicks + 1 ) * sessionPrice );
+  }
+  const subtractSeat = () => {
+    setClicks( clicks - 1 );
+    setPrice( ( clicks * sessionPrice ) - sessionPrice );
+  }
 
-useEffect(() => {
-  setPrice(sessionPrice * clicks)
-}, [sessionPrice, clicks])
+  useEffect(() => {
+    setPrice(sessionPrice * clicks)
+  }, [sessionPrice, clicks])
 
-  const n = 60; //butacas totales
-  
-  return (
-      <div className='flex flex-col items-center'>
+    const n = 60; //butacas totales
+    
+    return (
+      <section className='flex flex-col items-center'>
 
         <ul className='flex justify-center w-80 list-none bg-[#0000001a] text-[#777] rounded-md'>
-                <li className='flex p-2 list-none'>
-                    <div className='bg-[#444451] h-3 w-4 rounded-t-xl m-1'></div>
-                    <small>N/A</small>
-                </li>
-                <li className='flex p-2 list-none'>
-                    <div className='bg-[#6feaf6] h-3 w-4 rounded-t-xl m-1'></div>
-                    <small>Selected</small>
-                </li>
-                <li className='flex p-2 list-none'>
-                    <div className='bg-white h-3 w-4 rounded-t-xl m-1'></div>
-                    <small>Occupied</small>
-                </li>
-            </ul> 
+          <li className='flex p-2 list-none'>
+            <div className='bg-[#444451] h-3 w-4 rounded-t-xl m-1'></div>
+            <small>N/A</small>
+          </li>
+          <li className='flex p-2 list-none'>
+            <div className='bg-[#6feaf6] h-3 w-4 rounded-t-xl m-1'></div>
+            <small>Selected</small>
+          </li>
+          <li className='flex p-2 list-none'>
+            <div className='bg-white h-3 w-4 rounded-t-xl m-1'></div>
+            <small>Occupied</small>
+          </li>
+        </ul> 
         
         <div>
           <div className='flex m-2 w-52 flex-wrap justify-center'>
             <div className='bg-[#fff] w-full h-12 mb-6 text-center text-white shadow-md'>Screen</div>
             {[...Array(n)].map((e, i) => 
             <div className='bg-[#444451] h-3 w-4 m-1 rounded-t-xl transform transition duration-500 hover:scale-125 cursor-pointer'
-                key={i}
-                style={{backgroundColor: isSelected[`${i}`] ? '#6feaf6' :  ''}} 
-                onClick = { () => selectedSeat(i) }>
+              key={i}
+              style={{backgroundColor: isSelected[`${i}`] ? '#6feaf6' :  ''}} 
+              onClick = { () => selectedSeat(i) }>
             </div>)}
           </div>
         </div>
+
         <p className='text-white mt-3'>You have selected {clicks} seats for a price of {price}€</p>
-      </div>
-  )
+
+      </section>
+    )
 }
--- src/components/SessionSelect.jsx
+++ src/components/SessionSelect.jsx
@@ -5,51 +5,50 @@ import MoviesJson from '../movies.json'
 import { Cinema } from './Cinema'
 
 
-
 export const MovieSelect = () => {
 
-    const [dropdown, setDropdown] = useState(false)  
-    const openCloseDropdown = () => {
-        setDropdown(!dropdown)
-    }
+  const [dropdown, setDropdown] = useState(false)  
+  const openCloseDropdown = () => {
+    setDropdown(!dropdown)
+  }
 
-    const [price, setPrice] = useState(MoviesJson[0].price)
-    const sessionPrice = value => {
-        setPrice(value)
-    }
+  const [price, setPrice] = useState(MoviesJson[0].price)
+  const sessionPrice = value => {
+    setPrice(value)
+  }
 
-    const [name, setName] = useState(MoviesJson[0].name)
-    const sessionName = name => {
-        setName(name)
-    }
+  const [name, setName] = useState(MoviesJson[0].name)
+  const sessionName = name => {
+    setName(name)
+  }
 
-    return (
-        <section>
-            <Dropdown type = 'button' isOpen = {dropdown} toggle = {openCloseDropdown} className = 'flex justify-center items-center border-transparent mb-9' direction='right'>
-                <h5 className='mr-6 text-white'>Pick a Movie:</h5>
-                <DropdownToggle caret className='bg-white text-black w-56'>{ name }( { price }€ )<DropdownMenu>
-                        {
-                            MoviesJson.map( movie => {
-                                return (
-                                        <DropdownItem className='box' key={ movie.id } 
-                                        //[movie.name, movie.price]
-                                        value = {movie.price}
-                                        name = {movie.name} 
-                                        onClick = {e => { sessionPrice(e.target.value) ; sessionName(e.target.name) } }
-                                        size = 'sm'>
-                                            { movie.name }
-                                            ({ movie.price }€)
-                                        </DropdownItem>
-                                )}
-                            )
-                        }
-                </DropdownMenu>
-            </DropdownToggle >
-            </Dropdown>
-            
-            <Cinema sessionPrice={price} sessionName={name}/>
+  return (
+    <section>
+      <Dropdown type = 'button' isOpen = {dropdown} toggle = {openCloseDropdown} className = 'flex justify-center items-center border-transparent mb-9' direction='right'>
+        <h5 className='mr-6 text-white'>Pick a Movie:</h5>
+        <DropdownToggle caret className='bg-white text-black w-56'>{ name }( { price }€ )
+          <DropdownMenu>
+            {
+              MoviesJson.map( movie => {
+                return (
+                  <DropdownItem className='box' key={ movie.id } 
+                    //[movie.name, movie.price]
+                    value = {movie.price}
+                    name = {movie.name} 
+                    onClick = {e => { sessionPrice(e.target.value) ; sessionName(e.target.name) } }
+                    size = 'sm'>
+                        { movie.name }
+                        ({ movie.price }€)
+                  </DropdownItem>
+                  )}
+                )
+            }
+          </DropdownMenu>
+        </DropdownToggle >
+      </Dropdown>
+          
+      <Cinema sessionPrice={price} />
         
-        </section>
-
+    </section>
     )
 }
