b49f709f035c42b1608d22f9f3460e6da6b7b35d
ginkgo <alexvid33@gmail.com>
2022-10-13 17:14:43 +0200

    card superposition when hover

--- src/components/MoviePoster.jsx
+++ src/components/MoviePoster.jsx
@@ -19,38 +19,31 @@ const MoviePoster = () => {
       <div className='grid grid-cols-5 content-center justify-items-center m-4'>
         {
           MoviesJson.map( (movie, i) => 
-        <div className='m-2 w-40 h-auto hover:scale-[1.7] transition duration-500' key={movie.id}> 
+        <div className='m-2 w-40 h-auto hover:scale-[1.7] transition duration-500 hover:z-10' key={movie.id}> 
           <div className='relative z-0 cursor-pointer group'>
             <img className='rounded-lg' src={movie.img} alt={movie.name} /> 
 
-                <div className='absolute text-[9px] text-ellipsis overflow-hidden text-white inset-0 z-10 w-full h-full hover:bg-zinc-900/90 transition duration-200 rounded-lg hidden group-hover:block' onMouseEnter={ () =>  hideText(i) }  onMouseLeave={ () => hideText(i) }>
-
-                  <div className='flex flex-col items-center p-2 font-semibold text-center'>
-                    <p className='text-[2vh] p-1'>{movie.name}</p>
-                      <p onClick={() => navigate ('/session')} className='text-black bg-white rounded-md p-1 w-24 hover:scale-110 transition duration-200'>
-                        Show Film Sessions
-                      </p>
-                  </div>
-
-                  <div className='flex justify-center items-center justify-evenly w-full bg-gray-900 p-1'>
-                    {/* <p className='text-center font-semibold text-yellow-500 rounded-full p-1'>{movie.rating}</p>  */}
-                    <p>{movie.year}</p>
-                    <p>{movie.running_time}</p>
-                  </div>
-
-                  <div className='flex flex-col justify-center p-2'>
-                    {/* <p>Director: {movie.director}</p> */}
-                    <p className='pb-1'>{movie.genre}</p>
-                    <hr style={{ color: "white", height: 3}}/>
-                    <p className='text-gray-300 pt-1'>{movie.shortsynopsis}</p>
-                  </div>
-
-                </div>
+            <div className='absolute text-[9px] text-ellipsis overflow-hidden text-white inset-0 z-10 w-full h-full hover:bg-zinc-900/90 transition duration-200 rounded-lg hidden group-hover:block' onMouseEnter={ () =>  hideText(i) }  onMouseLeave={ () => hideText(i) }>
+              <div className='flex flex-col items-center p-2 font-semibold text-center'>
+                <p className='text-[2vh] p-1'>{movie.name}</p>
+                  <p onClick={() => navigate ('/session')} className='text-black bg-white rounded-md p-1 w-24 hover:scale-110 transition duration-200'>
+                    Show Film Sessions
+                  </p>
+              </div>
+              <div className='flex justify-center items-center justify-evenly w-full bg-gray-900 p-1'>
+                {/* <p className='text-center font-semibold text-yellow-500 rounded-full p-1'>{movie.rating}</p>  */}
+                <p>{movie.year}</p>
+                <p>{movie.running_time}</p>
+              </div>
+              <div className='flex flex-col justify-center p-2'>
+                {/* <p>Director: {movie.director}</p> */}
+                <p className='pb-1'>{movie.genre}</p>
+                <hr style={{ color: "white", height: 3}}/>
+                <p className='text-gray-300 pt-1'>{movie.shortsynopsis}</p>
+              </div>
+            </div>
           </div>
-
-          <div key={i} style={{visibility:isHover[`${i}`] ? 'hidden' : 'visible'}}>
-            <p className='text-center text-gray-600' >{movie.name}</p>
-          </div> 
+          <p key={i} style={{visibility:isHover[`${i}`] ? 'hidden' : 'visible'}} className='text-center text-gray-600 p-2'>{movie.name}</p>
         </div>
         )
       }
